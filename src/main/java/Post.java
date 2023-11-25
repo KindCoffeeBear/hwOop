8c02861a3720e03dbefe51f4f96791ab6c8c8122
Vladislav Romanov <conter123@list.ru>
2023-11-25 17:09:06 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    //FormDate birthday = new FormDate();
+    public FormDate birthday;
+    public String name; // имя
+    public String passport; // паспорт
+    public String patronymic; // отчество
+    public String phone; // телефон
+    public String surname; // фамилия
+    boolean subscription; // подписка
+}
